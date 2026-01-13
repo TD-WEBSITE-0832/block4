5be0d2ab6da420febf11e2a7e03b04496a7f0bf0
TD-WEBSITE-0832 <abdullayvsaidxon76@gmail.com>
2026-01-14 00:03:31 +0500

    script

---
+++ srcript.js
@@ -0,0 +1,110 @@
+let killer="legend"
+console.log("killer")
+
+
+
+
+let youName="legend"
+console.log("youName")
+
+
+
+let year="legend"
+console.log("year")
+
+
+let my,real,years="legend"
+console.log("18")
+
+
+
+
+
+
+
+
+
+
+
+let age=18;
+let moth=2008.0516;
+console.log(age);
+console.log(moth);
+
+
+
+
+
+let firstName="website";
+console.log(firstName)
+
+
+
+
+let student;
+console.log(student)
+
+
+let id=Symbol('id')
+console.log(id)
+
+
+
+
+
+const alsHuge=BigInt(992032008)
+console.log(alsHuge)
+
+
+
+
+let ag=18;
+let FirstName='good'
+console.log(typeof FirstName)
+
+
+
+
+
+var yo=20;
+console.log(yo)
+yo=21;
+console.log(yo)
+
+
+
+
+let a=5, b=10;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(--b);
+console.log(--a);
+console.log(--b);
+console.log(--a);
+console.log(a++);
+console.log(a++);
+console.log(b++);
+console.log(a%b);
+
+
+
+
+
+
+
+
+
+const y=110
+const f=y>20;
+const d=y>200;
+
+
+
+
+
+
+
+
+
